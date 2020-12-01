f22cc29435c39571ccccc1becc1433080ff5df4b
StevanZ <stevan.zlatkovicc@gmail.com>
2020-12-01 13:17:27 +0100

    finished

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ const form = document.querySelector('form');
 const buttons = document.querySelectorAll('button');
 const cellsNumber = document.querySelector('.current-num-of-cells');
 const warningMessage = document.querySelector('.warning-message');
+const range = document.querySelector('.range');
 
 // current color
-let backgroundColor = 'black';
-
-
 // FUNCTIONS ********
+// // render table
 
-// render table
 const createCells = (numberOfCells) => {
   container.style.setProperty('--rows', numberOfCells);
   container.style.setProperty('--columns', numberOfCells);
@@ -22,63 +20,65 @@ const createCells = (numberOfCells) => {
   for(let i = 0; i < cells; i++) {
     const cell = document.createElement('div');
     cell.setAttribute('class', 'cell');
-    cell.style.opacity = "0.1";
-    container.appendChild(cell);
+    container.append(cell);
   }
   
-  cellsNumber.textContent = numberOfCells;
+  cellsNumber.textContent = `${numberOfCells} x ${numberOfCells}`;
 }
+
+// create default grid
+createCells(30);
+
+
 
 // color functions
-const randomColor = () => {
-  let r = Math.floor(Math.random() * 255);
-  let g = Math.floor(Math.random() * 255);
-  let b = Math.floor(Math.random() * 255);
-
-  let backgroundColor = `rgb(${r},${b},${g})`;
-  return backgroundColor;
-}
-
-
 const blackColor = () => {
-  let blackBackground = `rgb(0,0,0)`;
-  return blackBackground;
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach( cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = `rgb(0,0,0)`;
+      cell.style.opacity = "1";
+    });
+  });
 }
 
-const grayColor = (el) => {
+const randomColor = () => {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach( cell => {
+    cell.addEventListener('mouseover', (e) => {
+      let r = Math.floor(Math.random() * 255);
+      let g = Math.floor(Math.random() * 255);
+      let b = Math.floor(Math.random() * 255);
+      cell.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-  let grayBackgrooundColor = `rgb(0,0,0)`;
-  return grayBackgrooundColor;
+      cell.style.opacity = "1";
+    });
+  });
 }
 
+//call for  default color 
+blackColor();
 
-// change background of cells
-container.addEventListener('mouseover', (e) => {
-  if(e.target.classList.contains('cell')){
-    switch(backgroundColor) {
-      case 'black':
-        e.target.style.backgroundColor = blackColor();
-        e.target.style.opacity = "1";
-        break;
-      case 'gray':
-        e.target.style.backgroundColor = grayColor();
-        e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
-        break;
-      case 'random':
-        e.target.style.backgroundColor = randomColor();
-        e.target.style.opacity = "1"; 
-        break;  
-    }
-  }
-});
+
+const grayColor = () => {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach( cell => {
+    let gradient = 0;
+    cell.addEventListener('mouseover', (e) => {
+      function opacity () {
+        gradient += 0.1;
+        return gradient;
+      }
+      cell.style.backgroundColor = `rgba(0,0,0,${opacity()})`;
+    });
+  });
+}
+
 
 // reset board
 const resetCells = () => {
-  let cells = container.children;
-  let cellsArray = [].slice.call(cells);
-  cellsArray.forEach(cell => {
-    cell.style.backgroundColor = 'transparent';
-  });
+  container.innerHTML = '';
+  blackColor();
 }
 
 
@@ -86,50 +86,54 @@ const resetCells = () => {
 // reset btn
 resetBoard.addEventListener('click', () => {
   resetCells();
+  createCells(30);
+  blackColor();
   removeActiveClass();
-  backgroundColor = 'black';
   blackBackground.classList.add('active');
 });
 
+// remove active class
 const removeActiveClass = () => {
   buttons.forEach( btn => {
     btn.classList.remove('active');
   });
 }
 
-// color btn listeners
+
+// // COLOR BTN LISTENERS *********************
+
+// random background
 colorBackground.addEventListener('click', (e) => {
-  backgroundColor = 'random';
-
+  randomColor();
   removeActiveClass();
   e.target.classList.add('active');
 });
 
+
+// black background
 blackBackground.addEventListener('click', (e) => {
-  backgroundColor = 'black';
+  blackColor();
   removeActiveClass();
   e.target.classList.add('active');
 });
 
+
+// gradient background
 grayBackground.addEventListener('click', (e) => {
-  backgroundColor = 'gray';
+  grayColor();
   removeActiveClass();
   e.target.classList.add('active');
 });
 
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+range.addEventListener('change', (e) => {
   resetCells();
-
-  let cells = form[0].value;
-  form.reset();
-  if(cells > 3 && cells <= 100) {
-    createCells(cells);
-    warningMessage.textContent = '';
-  } else {
-    warningMessage.textContent = 'Please enter number between 4 and 100!';
-  }
+  createCells(e.target.value);
+  blackColor();
 });
 
-createCells(16);
+
+
+
+
+
